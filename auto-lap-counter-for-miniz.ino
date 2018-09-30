30d42e02306105fa79322e52ae439c5f545948a9
kan.notice <kan@notice.co.jp>
2018-09-30 14:20:55 +0900

    add lap signal LEDs

--- auto-lap-counter-for-miniz.ino
+++ auto-lap-counter-for-miniz.ino
@@ -3,11 +3,14 @@
  * author: kan
  */
 #define ANALOG_PORT 5
-#define ANALOG_THRESHOLD 1000
+#define ANALOG_THRESHOLD 800
 #define CHATTERING 2
 #define BOOING_LEVEL 3
 #define DELAY 40
 #define LAP 1
+
+#define RED_LED 9
+#define GREEN_LED 10
 
 unsigned long beforeTime = 0;
 unsigned long bestTime = 0xffffffff;
@@ -18,6 +21,9 @@ int debug = 0;
 int status = 0;
 int lapCount = 0;
 int booingLevel = 0;
+
+int lapSignalCount = 0;
+int lapSignalPort = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -49,10 +55,13 @@ void loop() {
         ++lapCount;
         Serial.print("lap:"); Serial.print(lapCount); Serial.print(" ");     
         printTime(interval);
+        lapSignalCount = 2;
         if (bestTime > interval) {
           bestTime = interval;
           Serial.print("You did it! the best LAP!\n");
+          lapSignalPort = GREEN_LED;
         } else {
+          lapSignalPort = RED_LED;
           ++booingLevel;
           if (booingLevel >= BOOING_LEVEL) {
             booingLevel = 0;
@@ -62,10 +71,17 @@ void loop() {
       }
     }
   }
+  if (lapSignalCount > 0) {
+    analogWrite(lapSignalPort, 255);
+    --lapSignalCount;
+    if (lapSignalCount == 0) {
+      analogWrite(lapSignalPort, 0);
+    }
+  }
   delay(DELAY);
 }
 
-void printTime(int time)
+void printTime(unsigned long time)
 {
   unsigned int secs = time / 1000;
   unsigned int msecs = (time / 10) % 100;
